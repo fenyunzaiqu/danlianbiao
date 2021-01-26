eeffb9dcc8e3d49bde92d952d03270c5cb4d682b
fyzq <974577177@qq.com>
2021-01-27 07:22:43 +0800

    4-6题的解答

--- danlianbiao/main.cpp
+++ danlianbiao/main.cpp
@@ -20,11 +20,16 @@ void showLinklist(Linklist L);
 void DeleteWithoutHead(Linklist &L, int x);//2.3.1 删除不带头节点的值为x的递归算法（即函数里面套函数，明确终止条件）
 void DeleteWithHead(Linklist &L, int x);//2.3.2 删除带头节点值为x的算法
 void Res_Print(Linklist L);//2.3.3 倒叙输出带头节点单链表的元素
-void DeleteMin(Linklist L);//2.3.4 带头节点单链表中删除唯一最小元素
+Linklist DeleteMin(Linklist &L);//2.3.4 带头节点单链表中删除唯一最小元素
+Linklist Reverse_1(Linklist L);//2.3.5.1逆置的第一种算法，将头节点摘下，然后依次用头插法插入，就达到逆置的效果
+Linklist Reverse_2(Linklist L);//2.3.5.2逆置的第二种算法，将前驱指针pre，后驱指针r反转
+void AscendSort(Linklist &L);//2.3.6 将单链表升序排序，还有一种是遍历访问完数据域，将他们存储到数组中后排序，然后再按顺序将数据重写入链表
 
 int main()
 {
     Linklist L=NULL;
+    List_TrailInsert(L);
+    showLinklist(L);
 /* 2.3.1题的输入输出测试
     List_HeadInsertWithoutHead(L);
     showLinklist(L);
@@ -37,12 +42,20 @@ int main()
     DeleteWithHead(L, 2);
     showLinklist(L);
 */
-    List_TrailInsert(L);
-    showLinklist(L);
+/* 2.3.3题的输入输出
     Res_Print(L);
-
+    showLinklist(L);
+*/
+/*2.3.4题
+    DeleteMin(L);
+*/
+/* 2.3.5题的不同两种算法
+    showLinklist(Reverse_1(L));
+    showLinklist(Reverse_2(L));
+*/
+    AscendSort(L);
+    showLinklist(L);
     //cout<<GetElem(L, 3)->data<<endl; 用移动指针的方式来访问数据
-    cout<<"hello"<<endl;
     return 0;
 }
 
@@ -84,7 +97,7 @@ Linklist List_TrailInsert(Linklist &L)
 {
     L=(Linklist)malloc(sizeof(Node));//因为不需要关心L->next指向哪里，所以也不需要初始化空链表了
     Node *s,*r=L;//需要多一个尾指针r来指向最后一个将要插入的元素，且要先申请L这个空间，再将L的地址赋值给r，
-    int A[]={2,2,1,3,2,3,2,2};
+    int A[]={1,3,1,2,3};
     for(int i=0;i<sizeof(A)/sizeof(int);i++)
     {
         s=(Node*)malloc(sizeof(Node));
@@ -171,4 +184,73 @@ void Res_Print(Linklist L)
         Res_Print(L->next);//这里可以把函数换成语句写进去，就可以发现最后的节点是最里面的循环，然后它的cout要第一个输出，然后从上到下，第一个节点的cout最后一个输出
     }
     if(L!=NULL) cout<<L->data<<" ";
+}
+
+//2.3.4 删除带头节点单链表中唯一的最小元素
+Linklist DeleteMin(Linklist &L)
+{
+    Node *p=L->next->next,*q=L->next,*pre=L,*minpre=L;//因为删除节点后，需要将删除节点的前后连接起来，所以要多用pre和minpre指针来做到。
+    while(p!=NULL)
+    {
+        if(p->data<q->data)
+        {
+            q=p;//删除节点q
+            minpre=pre;//删除节点q的前驱
+        }
+        pre=p;//更新pre指针和最小节点指针要在最后，注意顺序
+        p=p->next;
+    }
+    minpre->next=q->next;//将删除节点的前后连接起来
+    free(q);
+    return L;
+}
+
+//2.3.5.1逆置的第一种算法，将头节点摘下，然后依次用头插法插入，就达到逆置的效果
+Linklist Reverse_1(Linklist L)
+{
+    Node *p=L->next,*q=p->next;
+    L->next=NULL;
+    while(p!=NULL)
+    {
+        q=p->next;//q用来储存p的下一个节点（后继），这次需要插p进去。因为修改了p—>next,所以需要用q来储存
+        p->next=L->next;//因为是用的头插法，所以要把L本来在后面的插在p后面
+        L->next=p;//然后将p插在L后面
+        p=q;//最后将p节点向后移，
+    }
+    return L;
+}
+
+//2.3.5.2 通过三个相临的节点不断逆置指针指向来达到逆置的效果
+Linklist Reverse_2(Linklist L)
+{
+    Node *p=L->next,*r=p->next,*pre;
+    p->next=NULL;
+    while(r!=NULL)
+    {
+        pre=p;//遍历将p和r指针向后移，然后用pre储存p的位置
+        p=r;
+        r=r->next;
+        p->next=pre;//然后将指针反向指向
+    }
+    L->next=p;//终止循环的条件是p的下一个节点为NULL，然后将头节点与最后一个节点连接。
+    return L;
+}
+
+//2.3.6 升序排序单链表
+void AscendSort(Linklist &L)
+{
+    Node *p=L->next,*q=p->next,*pre;//pre是新建只有一个元素链表（抽象）的指针，本质上是L的第一个节点p断开了后面的链接，造成L变成了新建链表，而q储存着后面链表的信息
+    p->next=NULL;//以第一个节点构造只有一个节点的链表
+    p=q;//断开后将后面的“地址信息”重新给p
+    while(p!=NULL)
+    {
+        pre=L;//从头开始扫描，所以总要把pre置头节点
+        q=p->next;//因为这一轮循环会将原来第二个节点断开，所以要向后一位储存链表信息
+        while(pre->next!=NULL&&pre->next->data<p->data)
+            //当pre没遍历到最后一个节点，且只要是pre遍历到的data比第一个节点小的就继续遍历（找到比p节点大的接上），第一轮循环只有1，然后pre->next就为空了，发现p->data即原来的比它大，就退出循环
+            pre=pre->next;//遍历，到新建链表的尽头或者找到比p->data大的节点
+        p->next=pre->next;//在新建链表中，将比p->data大的节点接到p后面，即把比p大的放在p后面，然后pre节点比p小，pre->next比p大，这一步也是将p节点与原本的链表断开了，如果p正巧是新链表中最大的，那么这个p->next也是Null,被放在了队尾
+        pre->next=p;//然后将p节点放在pre后面，也就是p节点插入了pre（新链表中），上一个比它大，下一个比它小的指针
+        p=q;//然后p节点在原本链表向后移一位
+    }
 }
